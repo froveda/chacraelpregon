a500a897bf46b91b341d92c8447b560b4149e1f1
Fabricio Roveda <froveda@gmail.com>
2015-05-29 17:21:00 -0300

    Change email configuration

--- mail/mail.php
+++ mail/mail.php
@@ -24,7 +24,7 @@ if(isset($_POST['email'])) {
 	// or if you prefer/need to fall back to use PHP's inbuilt mail() function:
 	// $transport = Swift_MailTransport::newInstance();
 	
-	$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'TLS/SSL' )
+	$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'tls' )
 	  ->setUsername('vendemoschacra@gmail.com')     
 	  ->setPassword('Teteyaba13')
 	  ;
